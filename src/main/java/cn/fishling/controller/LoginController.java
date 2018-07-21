05bb453f78b513b51b2459bc03bedd9ab68ddfbc
76768 <767683908@qq.com>
2018-07-21 14:37:35 +0800

    修改配置

--- src/main/java/cn/fishling/controller/LoginController.java
+++ src/main/java/cn/fishling/controller/LoginController.java
@@ -1,8 +1,10 @@
 package cn.fishling.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,16 +12,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSON;
 
+import cn.fishling.service.LoginService;
+
 @Controller
 @RequestMapping("/loginController")
 public class LoginController {
+	@Autowired()
+	private LoginService loginService;
 
 	@ResponseBody
 	@RequestMapping("/showPerson")
 	public String showPersons(@RequestParam Map<String, String> map) {
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("1", "分分分");
-		resultMap.put("2", "2=帮我你无法你看我吉尔菲娜客服");
+		List<Map<String, Object>> resultMap = loginService.findUserList();;
+//		resultMap.put("1", "分分分");
+//		resultMap.put("2", "2=帮我你无法你看我吉尔菲娜客服");
 		return JSON.toJSONString(resultMap);
 	}
 }
