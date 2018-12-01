71f3f7cfe68dd089850c034fa120cd2a0f1916d6
yutao <767683908@qq.com>
2018-12-01 18:58:59 +0800

    删除im

--- src/main/java/cn/fishling/fishchat/pojo/message/MessageType.java
+++
@@ -1,9 +0,0 @@
-package cn.fishling.fishchat.pojo.message;
-
-/**
- * Created by pz on 16/11/23.
- */
-public class MessageType {
-    public static final String FRIEND = "friend";
-    public static final String GROUP ="group";
-}
